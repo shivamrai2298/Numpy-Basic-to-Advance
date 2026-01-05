726a07b551a707cf4ef60ce07823ab3096c236fd
Shivam Rai <shivamrai2298@gmail.com>
2026-01-05 10:54:33 +0530

    Create arrays_creation.py

---
+++ 01_basics/arrays_creation.py
@@ -0,0 +1,18 @@
+import numpy as np
+
+
+# From list
+arr1 = np.array([1, 2, 3, 4])
+
+
+# 2D array
+arr2 = np.array([[1, 2], [3, 4]])
+
+
+# Zeros, Ones, Range
+zeros = np.zeros((3, 3))
+ones = np.ones((2, 4))
+range_arr = np.arange(0, 10, 2)
+
+
+print(arr1, arr2, zeros, ones, range_arr)
